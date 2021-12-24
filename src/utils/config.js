cdf8402dbf22b895ce271130b89b8aeb126e47b5
vikash <vikashekma151299@gmail.com>
2021-12-24 10:25:20 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,23 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+    //TODO: ADD YOUR FIREBASE INFO HERE
+    
+      apiKey: "AIzaSyDrvK8qgZpDdztdgVHohKvKk9C2kIVLDn0",
+      authDomain: "gitapp-81555.firebaseapp.com",
+      databaseURL: "https://gitapp-81555-default-rtdb.firebaseio.com",
+      projectId: "gitapp-81555",
+      storageBucket: "gitapp-81555.appspot.com",
+      messagingSenderId: "552992740037",
+      appId: "1:552992740037:web:0ecdb9e177a49899f256ac",
+      measurementId: "G-41L3F5M4X7"
+    
+    
+  };
+  //image configuration
+  export const imageConfig = {
+    quality: 0.2,
+    maxWidth: 800,
+    maxHeight: 600,
+    autoRotate: true
+  };
+  
